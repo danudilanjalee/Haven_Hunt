471c9edb5ba14ec74c82d749fc624786019e36ca
danudilanjalee <139612457+danudilanjalee@users.noreply.github.com>
2024-12-09 15:32:01 +0000

    save changes in home file

--- home.php
+++ home.php
@@ -10,15 +10,15 @@
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-            <a class="navbar-brand" href="home.html">HavenHunt</a>
+            <a class="navbar-brand" href="home.php">HavenHunt</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
-                    <li class="nav-item"><a class="nav-link" href="home.html">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
-                    <li class="nav-item"><a class="btn btn-primary text-white" href="details.html">Enter Details</a></li>
+                    <li class="nav-item"><a class="nav-link" href="home.php">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
+                    <li class="nav-item"><a class="btn btn-primary text-white" href="details.php">Enter Details</a></li>
                 </ul>
             </div>
         </div>
