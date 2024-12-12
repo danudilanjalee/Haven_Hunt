7f17d0474eb3e24ed962abbc9e69074d68a62e53
danudilanjalee <139612457+danudilanjalee@users.noreply.github.com>
2024-12-12 16:29:13 +0000

    created data base

--- connection/connectdb.php
+++ connection/connectdb.php
@@ -7,14 +7,4 @@
 
     $connection = mysqli_connect($hostname,$username,$password,$dbname);
 ?>
-CREATE DATABASE havenhunt;
 
-USE havenhunt;
-
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
